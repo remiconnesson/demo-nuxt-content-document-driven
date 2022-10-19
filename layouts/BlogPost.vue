c2b9468fdad7a09ea0d501dbf6a6f1d283baab44
Rémi Connesson <remiconnesson@gmail.com>
2022-10-19 11:23:19 +0200

    Add navigation to blog-post layout

--- layouts/BlogPost.vue
+++ layouts/BlogPost.vue
@@ -1,7 +1,19 @@
+<script setup>
+const { next } = useContent()
+</script>
+
 <template>
 	<div class="container">
 		<div class="content">
 			<slot />
+		</div>
+		<div v-if="next" class="next-link">
+			<div>
+				<NuxtLink :to="next._path">
+				  {{ next.title }}
+				</NuxtLink>
+				<p> {{ next.description }} </p>
+			</div>
 		</div>
 	</div>
 </template>
